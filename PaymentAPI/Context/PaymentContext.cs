5d44723f09112e01b35e0c73e7b8ae2d6661e268
Dhiraj Kumar <dkumar@paycor.com>
2021-08-10 14:18:26 +0530

    Initial commit for the Payment API project with CRUD operations

---
+++ PaymentAPI/Context/PaymentContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Context
+{
+    public class PaymentContext : DbContext
+    {
+        public PaymentContext(DbContextOptions<PaymentContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
